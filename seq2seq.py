02ccb2fc802e2805646ada18123d0669044e9adb
苏剑林(Jianlin Su) <bojone@spaces.ac.cn>
2019-06-20 21:55:56 +0800

    Update seq2seq.py

--- seq2seq.py
+++ seq2seq.py
@@ -77,20 +77,6 @@ def data_generator():
                 X,Y = [],[]
 
 
-class OurLayer(Layer):
-    """定义新的Layer，增加reuse方法，允许在定义Layer时调用现成的层
-    """
-    def reuse(self, layer, *args, **kwargs):
-        if not layer.built:
-            if len(args) > 0:
-                layer.build(K.int_shape(args[0]))
-            else:
-                layer.build(K.int_shape(kwargs['inputs']))
-            self._trainable_weights.extend(layer._trainable_weights)
-            self._non_trainable_weights.extend(layer._non_trainable_weights)
-        return layer.call(*args, **kwargs)
-
-
 def to_one_hot(x):
     """输出一个词表大小的向量，来标记该词是否在文章出现过
     """
